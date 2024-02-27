5a0694f020d966330222169a8362835a4df0d158
abhijit <amiabhijit54@gmail.com>
2024-02-27 15:50:12 +0530

    This is the initial commit, no problem has been solved yet.

---
+++ Weird_Algorithm.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define wl            ll t;cin>>t;while(t--)
+#define rep(a,b,c)    for(ll a = b; a<c; a++)
+#define mp            make_pair
+#define vi            vector<ll>
+#define pb            push_back
+#define f_min         *min_element
+#define f_max         *max_element
+#define all(v)        v.begin(), v.end()
+#define endl          "\n"
+#define ll            long long int 
+#define ld            long double
+#define pi            pair<ll , ll>
+#define ull           unsigned long long
+#define ps(x,y)       fixed<<setprecision(x)<<y
+#define li            long int
+#define frs           first
+#define sec           second
+#define inf           INT_MAX
+#define setbits(x)    __builtin_popcountll(x)
+#define trailz(x)     __builtin_ctzll(x)
+#define pqnor         priority_queue<ll>
+#define pqrev         priority_queue<ll, vi, greater<ll>>
+#define ninf          INT_MIN
+#define ul            unsigned long int
+#define fast          std::ios::sync_with_stdio(false); cin.tie(NULL);
+const ll mod = 1e9 + 7;
+const ll N = 1e6 + 5;
+using namespace std;
+
+void solve(){
+    
+}   
+    
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    fast
+    wl
+    {
+        solve();
+    }
+    return 0;
+}
